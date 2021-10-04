19711da6652cd9da2a181007c040479b74b42985
Lorent Lempereur <lorent@commuty.net>
2021-10-04 18:07:44 +0200

    Upgrade ruby (2.7.4) and dependencies

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 require "sentry-ruby"
 if !Rails.env == "development" and !Rails.env == "test" and ENV.has_key?("SENTRY_DSN")
-  Sentry.configure do |config|
+  Sentry.init do |config|
     config.dsn                  = ENV["SENTRY_DSN"]
     config.enabled_environments = %w[ qa staging production ]
     config.send_default_pii     = true
